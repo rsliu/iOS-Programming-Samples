b369ebc07253b1d4d82e80340e337f4219f73028
Ren-Shiou Liu <rsliu.tw@yahoo.com.tw>
2014-01-21 02:59:53 +0800

    Mochismo (Lecture 3)

---
+++ MochismoV2/MochismoV2/CardGameViewController.m
@@ -0,0 +1,60 @@
+//
+//  CardGameViewController.m
+//  Mochismo
+//
+//  Created by Ren-Shiou Liu on 1/19/14.
+//  Copyright (c) 2014 Ren-Shiou Liu. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+// It is sort of unfortunate that we are importing PlayingCardDeck into this class since it is otherwise a generic card matching game Controller.
+//In other words, there’s really nothing that would prevent it from working with other Decks of other kinds of cards than PlayingCards.
+//We’ll use polymorphism next week to improve this state of affairs.
+#import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
+
+@interface CardGameViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label; // label for displaying score
+@property (strong, nonatomic) CardMatchingGame* game; // need a property for the model
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@end
+
+@implementation CardGameViewController
+
+
+-(Deck*) createDeck {
+    return [[PlayingCardDeck alloc] init];
+}
+
+-(CardMatchingGame*) game {
+    if (!_game) {
+        _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count] usingDeck:[self createDeck]];
+    }
+    
+    return _game;
+}
+
+// Button click event handler
+- (IBAction)doFlipCard:(UIButton *)sender {
+    // Find out the index of the button being clicked
+    int choosenIndex = [self.cardButtons indexOfObject:sender];
+    // Call the model to choose the card at that index
+    [self.game chooseCardAtIndex:choosenIndex];
+    // Update the UI according to the new state of the model
+    [self updateUI];
+}
+
+-(void) updateUI {
+    for(UIButton* cardButton in self.cardButtons) {
+        // Find out card index
+        int cardIndex = [self.cardButtons indexOfObject:cardButton];
+        // Get the card object
+        Card* card = [self.game cardAtIndex:cardIndex];
+        [cardButton setTitle:((card.isChosen)? card.contents:@"") forState:UIControlStateNormal];
+        UIImage* image = [UIImage imageNamed:((card.isChosen)? @"BlankCard":@"stanford")];
+        [cardButton setBackgroundImage:image forState:UIControlStateNormal];
+        cardButton.enabled = !card.isMatched;
+    }
+}
+@end
---
+++ MochismoV2/MochismoV2/CardMatchingGame.h
@@ -0,0 +1,19 @@
+//
+//  CardMatchingGame.h
+//  MochismoV2
+//
+//  Created by Ren-Shiou Liu on 1/21/14.
+//  Copyright (c) 2014 Ren-Shiou Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+#import "Card.h"
+
+@interface CardMatchingGame : NSObject
+
+@property (nonatomic, readonly) NSUInteger score; // Make sure no one can change this public property
+- (instancetype) initWithCardCount:(int) count usingDeck:(Deck*) deck; // Dedicated initializer must be public
+- (void) chooseCardAtIndex:(NSUInteger) index; // Method for choosing a card
+- (Card*) cardAtIndex:(NSUInteger) index; // Method for accessing a card
+@end

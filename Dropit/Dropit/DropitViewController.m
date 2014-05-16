db94f16dbd9cfd4fa475adb9c0ac341b88515370
Ren-Shiou Liu <renshiou.liu@gmail.com>
2014-05-16 14:13:41 +0800

    Align blocks when they fall

--- Dropit/Dropit/DropitViewController.m
+++ Dropit/Dropit/DropitViewController.m
@@ -29,20 +29,7 @@
     
     if (sender.state == UIGestureRecognizerStateBegan) {
         // Attach the dropping view to the point
-        if (self.droppingView) {
-            self.attachment = [[UIAttachmentBehavior alloc] initWithItem:self.droppingView attachedToAnchor:gesturePoint];
-            UIView *droppingView = self.droppingView;
-            __weak DropitViewController *weakSelf = self;
-            // The dynamic animator will call this block on every animation step
-            self.attachment.action = ^{
-                UIBezierPath* path = [[UIBezierPath alloc] init];
-                [path moveToPoint:weakSelf.attachment.anchorPoint];
-                [path addLineToPoint:droppingView.center];
-                weakSelf.gameView.path = path;
-            };
-            self.droppingView = nil; // only one shot
-            [self.animator addBehavior:self.attachment]; // Add to behavior
-        }
+        [self attachDroppingViewToPoint:gesturePoint];
     } else if (sender.state == UIGestureRecognizerStateChanged) {
         // Update the anchor point as the user moves along
         self.attachment.anchorPoint = gesturePoint;
@@ -50,6 +37,24 @@
         // Stop the behavior when the gesture stops
         [self.animator removeBehavior:self.attachment];
         self.gameView.path = nil;
+    }
+}
+
+-(void) attachDroppingViewToPoint:(CGPoint) anchorPoint
+{
+    if (self.droppingView) {
+        self.attachment = [[UIAttachmentBehavior alloc] initWithItem:self.droppingView attachedToAnchor:anchorPoint];
+        UIView *droppingView = self.droppingView;
+        __weak DropitViewController *weakSelf = self;
+        // The dynamic animator will call this block on every animation step
+        self.attachment.action = ^{
+            UIBezierPath* path = [[UIBezierPath alloc] init];
+            [path moveToPoint:weakSelf.attachment.anchorPoint];
+            [path addLineToPoint:droppingView.center];
+            weakSelf.gameView.path = path;
+        };
+        self.droppingView = nil; // only one shot
+        [self.animator addBehavior:self.attachment]; // Add to behavior
     }
 }
 

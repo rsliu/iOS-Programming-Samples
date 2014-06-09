c90eff68c14ac567e700979932fa3ffa1cc9e7dc
Ren-Shiou Liu <renshiou.liu@gmail.com>
2014-06-09 22:28:27 +0800

    Photomania with map (iPhone only)

---
+++ Photomania/Photomania/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"71746a2c79d760200f676ffd4728dd24"
